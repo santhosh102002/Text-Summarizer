af0a8215a7d2b063a685c6adfd632226967f312a
KADIYAM SANTHOSH KUMAR <santhosh102002@gmail.com>
2023-10-26 18:42:46 +0530

    setup package

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import setup
 
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
 
+AUTHOR_USER_NAME = "santhosh102002"
+REPO_NAME = "Text-Summarizer"
 setup(
-    name="src",
+    name="textsummarizer",
     version="0.0.0",
     author="Santhosh Kumar Kadiyam",
     author_email="santhosh102002@gmail.com",
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github/com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+
+    },
+
     
 )
